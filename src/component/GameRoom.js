6418209075bce01bb37e7bc47c53f0040fb117b6
Playneko <playneko@outlook.com>
2024-11-09 09:52:49 +0900

    React Gostop

--- src/component/GameRoom.js
+++ src/component/GameRoom.js
@@ -12,7 +12,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const drawMiddleBoard = () => {
+const drawClearBoard = () => {
+  console.log("xxxx");
   let canvas = document.getElementById("board");
   let ctx = canvas.getContext("2d");
   ctx.fillStyle = "#8bc34a";
@@ -31,15 +32,21 @@ const drawImageBoard = (card, x, y) => {
 }
 
 const GameRoom = (props) => {
-  const cardDataList = ['pae0.png','pae1.png','pae2.png','pae3.png',
-  'pae4.png','pae5.png','pae6.png','pae7.png','pae8.png','pae9.png',
-  'pae10.png','pae11.png','pae12.png','pae13.png','pae14.png','pae15.png',
-  'pae16.png','pae17.png','pae18.png','pae19.png','pae20.png','pae21.png',
-  'pae22.png','pae23.png','pae24.png','pae25.png','pae26.png','pae27.png',
-  'pae28.png','pae29.png','pae30.png','pae31.png','pae32.png','pae33.png',
-  'pae34.png','pae35.png','pae36.png','pae37.png','pae38.png','pae39.png',
-  'pae40.png','pae41.png','pae42.png','pae43.png','pae44.png','pae45.png',
-  'pae46.png','pae47.png'];
+  // 광(20), 열끗(10), 띠(5), 피(1)
+  const cardDataList = [
+    {image: 'pae0.png', kind: 20, card: 0, match: 1},{image: 'pae1.png', kind: 5, card: 1, match: 1},{image: 'pae2.png', kind: 1, card: 2, match: 1},{image: 'pae3.png', kind: 1, card: 3, match: 1},
+    {image: 'pae4.png', kind: 10, card: 4, match: 2},{image: 'pae5.png', kind: 5, card: 5, match: 2},{image: 'pae6.png', kind: 1, card: 6, match: 2},{image: 'pae7.png', kind: 1, card: 7, match: 2},
+    {image: 'pae8.png', kind: 20, card: 8, match: 3},{image: 'pae9.png', kind: 5, card: 9, match: 3},{image: 'pae10.png', kind: 1, card: 10, match: 3},{image: 'pae11.png', kind: 1, card: 11, match: 3},
+    {image: 'pae12.png', kind: 10, card: 12, match: 4},{image: 'pae13.png', kind: 5, card: 13, match: 4},{image: 'pae14.png', kind: 1, card: 14, match: 4},{image: 'pae15.png', kind: 1, card: 15, match: 4},
+    {image: 'pae16.png', kind: 10, card: 16, match: 5},{image: 'pae17.png', kind: 5, card: 17, match: 5},{image: 'pae18.png', kind: 1, card: 18, match: 5},{image: 'pae19.png', kind: 1, card: 19, match: 5},
+    {image: 'pae20.png', kind: 10, card: 20, match: 6},{image: 'pae21.png', kind: 5, card: 21, match: 6},{image: 'pae22.png', kind: 1, card: 22, match: 6},{image: 'pae23.png', kind: 1, card: 23, match: 6},
+    {image: 'pae24.png', kind: 10, card: 24, match: 7},{image: 'pae25.png', kind: 5, card: 25, match: 7},{image: 'pae26.png', kind: 1, card: 26, match: 7},{image: 'pae27.png', kind: 1, card: 27, match: 7},
+    {image: 'pae28.png', kind: 20, card: 28, match: 8},{image: 'pae29.png', kind: 10, card: 29, match: 8},{image: 'pae30.png', kind: 1, card: 30, match: 8},{image: 'pae31.png', kind: 1, card: 31, match: 8},
+    {image: 'pae32.png', kind: 10, card: 32, double: 1, match: 9},{image: 'pae33.png', kind: 5, card: 33, match: 9},{image: 'pae34.png', kind: 1, card: 34, match: 9},{image: 'pae35.png', kind: 1, card: 35, match: 9},
+    {image: 'pae36.png', kind: 10, card: 36, match: 10},{image: 'pae37.png', kind: 5, card: 37, match: 10},{image: 'pae38.png', kind: 1, card: 38, match: 10},{image: 'pae39.png', kind: 1, card: 39, match: 10},
+    {image: 'pae40.png', kind: 20, card: 40, match: 11},{image: 'pae41.png', kind: 1, card: 41, double: 1, match: 11},{image: 'pae42.png', kind: 1, card: 42, match: 11},{image: 'pae43.png', kind: 1, card: 43, match: 11},
+    {image: 'pae44.png', kind: 20, card: 44, match: 12},{image: 'pae45.png', kind: 1, card: 45, match: 12},{image: 'pae46.png', kind: 5, card: 46, match: 12},{image: 'pae47.png', kind: 1, card: 47, double: 1, match: 12}
+  ];
   const cardXyList = [
     [15, 140], [75, 140], [135, 140], 
     [15,220], [75,220], [135,220], 
@@ -47,16 +54,28 @@ const GameRoom = (props) => {
     [225,220], [285,220], [345,220]
   ];
   const [gameStart, setGameStart] = React.useState(false);
+  const [waitCardList, setWaitCardList] = React.useState([
+    {image: 'pae2.png', kind: 1, card: 2, match: 1},{image: 'pae4.png', kind: 10, card: 4, match: 2},{image: 'pae5.png', kind: 5, card: 5, match: 2},
+    {image: 'pae7.png', kind: 1, card: 7, match: 2},{image: 'pae8.png', kind: 20, card: 8, match: 3},{image: 'pae9.png', kind: 5, card: 9, match: 3},
+    {image: 'pae13.png', kind: 5, card: 13, match: 4},{image: 'pae14.png', kind: 1, card: 14, match: 4},{image: 'pae16.png', kind: 10, card: 16, match: 5},
+    {image: 'pae17.png', kind: 5, card: 17, match: 5},{image: 'pae28.png', kind: 20, card: 28, match: 8},{image: 'pae29.png', kind: 10, card: 29, match: 8},
+    {image: 'pae30.png', kind: 1, card: 30, match: 8},{image: 'pae31.png', kind: 1, card: 31, match: 8},{image: 'pae32.png', kind: 10, card: 32, double: 1, match: 9},
+    {image: 'pae33.png', kind: 5, card: 33, match: 9},{image: 'pae34.png', kind: 1, card: 34, match: 9},{image: 'pae35.png', kind: 1, card: 35, match: 9},
+    {image: 'pae37.png', kind: 5, card: 37, match: 10},{image: 'pae38.png', kind: 1, card: 38, match: 10},{image: 'pae41.png', kind: 1, card: 41, double: 1, match: 11},
+    {image: 'pae42.png', kind: 1, card: 42, match: 11},{image: 'pae43.png', kind: 1, card: 43, match: 11},{image: 'pae44.png', kind: 20, card: 44, match: 12},
+    {image: 'pae45.png', kind: 1, card: 45, match: 12},{image: 'pae47.png', kind: 1, card: 47, double: 1, match: 12}
+  ]);
   const [boardCardList, setBoardCardList] = React.useState([
-    'pae1', 'pae18', 'pae22',
-    'pae39', 'pae40', 'pae25',
-    'pae0', 'pae10', 'pae15',
-    'pae23', 'pae24', 'pae46'
+    {image: 'pae1', kind: 5, card: 1, match: 1},{image: 'pae18', kind: 1, card: 18, match: 5},{image: 'pae22', kind: 1, card: 22, match: 6},
+    {image: 'pae39', kind: 1, card: 39, match: 10},{image: 'pae40', kind: 20, card: 40, match: 11},{image: 'pae25', kind: 5, card: 25, match: 7},
+    {image: 'pae0', kind: 20, card: 0, match: 1},{image: 'pae10', kind: 1, card: 10, match: 3},{image: 'pae15', kind: 1, card: 15, match: 4},
+    {image: 'pae23', kind: 1, card: 23, match: 6},{image: 'pae24', kind: 10, card: 24, match: 7},{image: 'pae46', kind: 5, card: 46, match: 12}
   ]);
   const [myCardList, setMyCardList] = React.useState([
-    'pae6', 'pae19', 'pae20', 'pae36',
-    'pae27', 'pae26', 'pae3', 'pae12',
-    'pae11', 'pae21'
+    {image: 'pae6', kind: 1, card: 6, match: 2},{image: 'pae19', kind: 1, card: 19, match: 5},{image: 'pae20', kind: 10, card: 20, match: 6},
+    {image: 'pae36', kind: 10, card: 36, match: 10},{image: 'pae27', kind: 1, card: 27, match: 7},{image: 'pae26', kind: 1, card: 26, match: 7},
+    {image: 'pae3', kind: 1, card: 3, match: 1},{image: 'pae12', kind: 10, card: 12, match: 4},{image: 'pae11', kind: 1, card: 11, match: 3},
+    {image: 'pae21', kind: 5, card: 21, match: 6}
   ]);
   const classes = useStyles();
 
@@ -64,7 +83,7 @@ const GameRoom = (props) => {
     setGameStart(true);
     boardCardList.map((item, idx) => (
       // console.log(cardXyList[idx][0])
-      drawImageBoard(item, cardXyList[idx][0], cardXyList[idx][1])
+      drawImageBoard(item.image, cardXyList[idx][0], cardXyList[idx][1])
     ));
   }
 
@@ -72,70 +91,7 @@ const GameRoom = (props) => {
   // CheckLogin(props);
 
   useEffect(() => {
-    drawMiddleBoard();
-
-    // 오광 출력
-    // drawImageBoard("pae0", 5, 40);
-    // drawImageBoard("pae8", 15, 40);
-    // drawImageBoard("pae28", 25, 40);
-    // drawImageBoard("pae40", 35, 40);
-    // drawImageBoard("pae44", 45, 40);
-
-    // 열끗 출력
-    // drawImageBoard("pae4", 85, 10);
-    // drawImageBoard("pae45", 95, 10);
-    // drawImageBoard("pae36", 105, 10);
-
-    // drawImageBoard("pae29", 140, 10);
-    // drawImageBoard("pae24", 150, 10);
-    // drawImageBoard("pae20", 160, 10);
-
-    // drawImageBoard("pae16", 195, 10);
-    // drawImageBoard("pae12", 205, 10);
-
-    // 띠 출력
-    // drawImageBoard("pae1", 85, 40);
-    // drawImageBoard("pae5", 95, 40);
-    // drawImageBoard("pae9", 105, 40);
-
-    // drawImageBoard("pae21", 140, 40);
-    // drawImageBoard("pae33", 150, 40);
-    // drawImageBoard("pae37", 160, 40);
-
-    // drawImageBoard("pae13", 195, 40);
-    // drawImageBoard("pae17", 205, 40);
-    // drawImageBoard("pae25", 215, 40);
-    // drawImageBoard("pae46", 225, 40);
-
-    // 피 출력
-    // drawImageBoard("pae2", 265, 10);
-    // drawImageBoard("pae3", 275, 10);
-    // drawImageBoard("pae6", 285, 10);
-    // drawImageBoard("pae7", 295, 10);
-    // drawImageBoard("pae10", 305, 10);
-    // drawImageBoard("pae11", 315, 10);
-    // drawImageBoard("pae14", 325, 10);
-    // drawImageBoard("pae15", 335, 10);
-    // drawImageBoard("pae18", 345, 10);
-    // drawImageBoard("pae19", 355, 10);
-    // drawImageBoard("pae22", 365, 10);
-
-    // drawImageBoard("pae23", 265, 40);
-    // drawImageBoard("pae26", 275, 40);
-    // drawImageBoard("pae27", 285, 40);
-    // drawImageBoard("pae30", 295, 40);
-    // drawImageBoard("pae31", 305, 40);
-    // drawImageBoard("pae34", 315, 40);
-    // drawImageBoard("pae35", 325, 40);
-    // drawImageBoard("pae38", 335, 40);
-    // drawImageBoard("pae39", 345, 40);
-    // drawImageBoard("pae42", 355, 40);
-    // drawImageBoard("pae43", 365, 40);
-
-    // 쌍피 출력
-    // drawImageBoard("pae47", 300, 20);
-    // drawImageBoard("pae32", 310, 20);
-    // drawImageBoard("pae41", 320, 20);
+    drawClearBoard();
   }, []);
 
   return (
@@ -154,7 +110,7 @@ const GameRoom = (props) => {
           {
             myCardList.map((item, idx) => (
               // console.log(item)
-              <img id={"my-pae" + idx} src={"/images/" + item + ".png"} />
+              <img id={"my-pae" + idx} src={"/images/" + item.image + ".png"} />
             ))
           }
           </div>
@@ -163,7 +119,7 @@ const GameRoom = (props) => {
       }
       {
         cardDataList.map((item, idx) => (
-          <img id={"pae" + idx} src={"/images/" + item} />
+          <img id={"pae" + idx} src={"/images/" + item.image} />
         ))
       }
     </div>
